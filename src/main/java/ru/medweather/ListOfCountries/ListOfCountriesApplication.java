89e3faf2e0cad96a27a7350cb94e025ac29fa0dc
Alexandr Medvedev <medweather@mail.ru>
2019-11-05 12:06:50 +0300

    initial project

---
+++ src/main/java/ru/medweather/ListOfCountries/ListOfCountriesApplication.java
@@ -0,0 +1,13 @@
+package ru.medweather.ListOfCountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListOfCountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListOfCountriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/medweather/ListOfCountries/ListOfCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.medweather.ListOfCountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListOfCountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
